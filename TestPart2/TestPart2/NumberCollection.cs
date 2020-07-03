2046137e0e9887b4dd90da9fcf69cbf17fe34b41
Yoav <47596599+ChaoSpiritZ@users.noreply.github.com>
2020-07-03 14:33:58 +0300

    Add files via upload

---
+++ TestPart2/TestPart2/NumberCollection.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPart2
+{
+    class NumberCollection
+    {
+        List<int> _numbers = new List<int>();
+        private int? _highestNumber = null;
+        private int? _lowestNumber = null;
+
+
+        public void Push(int number)
+        {
+            _numbers.Insert(0, number);
+            _highestNumber = _numbers.Max();
+            _lowestNumber = _numbers.Min();
+        }
+
+        public int? Pop()
+        {
+            if (_numbers.Count > 0)
+            {
+                int result = _numbers[0];
+                _numbers.RemoveAt(0);
+
+                if (_numbers.Count > 0)
+                {
+                    _highestNumber = _numbers.Max(); //seems too easy hmmmmmmm.....
+                    _lowestNumber = _numbers.Min();
+                }
+                else
+                {
+                    _highestNumber = null;
+                    _lowestNumber = null;
+                }
+
+                return result;
+            }
+            else
+                return null;
+        }
+
+        public int? Peep()
+        {
+            if (_numbers.Count > 0)
+                return _numbers[0];
+            else
+                return null;
+        }
+
+        public void PrintAll()
+        {
+            Console.WriteLine("--------list--------");
+            for (int i = _numbers.Count; i > 0; i--)
+            {
+                Console.Write(_numbers[i - 1] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Highest: {_highestNumber}");
+            Console.WriteLine($"Lowest: {_lowestNumber}");
+            Console.WriteLine("--------------------");
+        }
+    }
+}
---
+++ TestPart2/TestPart2/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPart2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //question 1
+            //List<string> javaList = new List<string>() { "java", "jjava", "vaj", "aavj", "j", "vjaa", "dan", "and", "ddan" };
+            //while (true)
+            //    Console.WriteLine(RepeatCount(javaList, Console.ReadLine()));
+
+            //question 2
+            while (true)
+                Push5Pop5();
+        }
+
+        /// <summary>
+        /// Question 1
+        /// </summary>
+        /// <param name="strings"></param>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        static int RepeatCount(List<string> strings, string word)
+        {
+            int counter = 0;
+
+            string wordClone = word; //thought about stringbuilder but it doesn't have some methods that string has
+
+            for (int i = 0; i < strings.Count; i++)
+            {
+                counter++;
+                foreach (var letter in strings[i])
+                {
+                    if (wordClone.Contains(letter))
+                    {
+                        wordClone = wordClone.Remove(wordClone.IndexOf(letter), 1);
+                    }
+                    else
+                    {
+                        counter--;
+                        wordClone = ""; //reset here so the length checker won't reduce the counter twice, weird workaround but it works
+                        break;
+                    }
+                }
+                if (wordClone.Length != 0)
+                    counter--;
+                wordClone = word;
+            }
+
+
+
+            return counter;
+        }
+
+        public static void Push5Pop5()
+        {
+            NumberCollection nums = new NumberCollection();
+            nums.PrintAll();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("enter a number to push, anything that is not a number is considered 0:");
+                int enteredNumber = 0;
+                int.TryParse(Console.ReadLine(), out enteredNumber);
+                nums.Push(enteredNumber);
+                nums.PrintAll();
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("press ENTER to pop the next number");
+                Console.ReadLine();
+                Console.WriteLine("popped " + nums.Pop());
+
+                nums.PrintAll();
+            }
+        }
+    }
+}
